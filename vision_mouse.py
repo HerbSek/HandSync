548cb6bebb013b37afdad6093bd8f2a7a80da77c
Herbert_dev <sekpeyherbert@gmail.com>
2024-07-03 15:03:30 +0000

    Done with basic mouse functionalities!!!

--- vision_mouse.py
+++ vision_mouse.py
@@ -3,20 +3,18 @@ import mediapipe as mp
 import pyautogui
 import time
 
-def landmarks_close(lm1, lm2, threshold=0.10):
+def landmarks_close(lm1, lm2, threshold=0.07):
     return abs(lm1.x - lm2.x) < threshold and abs(lm1.y - lm2.y) < threshold and abs(lm1.z - lm2.z) < threshold
 
-
+pyautogui.FAILSAFE = False
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
 time.sleep(5)
-
 screen_width, screen_height = pyautogui.size()
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
-        model_complexity=1,
+        model_complexity=1,  
         min_detection_confidence=0.6,
         min_tracking_confidence=0.6) as hands:
     
@@ -27,8 +25,6 @@ with mp_hands.Hands(
             continue
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
         image = cv2.flip(image, 1)
 
         copy_image = image.copy()
@@ -37,7 +33,6 @@ with mp_hands.Hands(
 
         image = cv2.resize(image, (int(screen_width * 1.3), int(screen_height * 1.3)), interpolation = cv2.INTER_AREA)
 
-    
         results = hands.process(image)    
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -51,7 +46,6 @@ with mp_hands.Hands(
                     mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
                     )
             
-
                 for id, landmark in enumerate(hand_landmarks.landmark):
                     height, width, _ = image.shape
                     cx, cy = int(landmark.x * width), int(landmark.y * height)
@@ -59,8 +53,7 @@ with mp_hands.Hands(
 
                     # Optionally draw the coordinates on the image
                     # cv2.putText(image, f'{id}', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
-                   
-                   
+                
                     if not(landmarks_close(hand_landmarks.landmark[4], hand_landmarks.landmark[8])) :  # Example: Move the mouse to the 9th landmark (index finger tip)
                         if id == 8:
                             cv2.circle(copy_image, (cx//5,cy//5), 1, (255, 0, 0), 3)
@@ -69,22 +62,15 @@ with mp_hands.Hands(
                             # time.sleep(0.1)  # Adding a small delay
                     elif landmarks_close(hand_landmarks.landmark[4], hand_landmarks.landmark[8]) and id == 8:
                         # if hand_landmarks.landmark[4] ==  hand_landmarks.landmark[8]:
-                        pyautogui.dragTo(cx, cy) 
-                        print(f'User is dragging !!!')
-                       
-                    
-                   
-                    
-
-
-        
+                        # pyautogui.click(cx, cy) 
+                        pyautogui.dragTo(cx,cy)
+                        print(f'User is dragging !!!') 
+                               
         # cv2.imshow('Hand Tracking', image)
         cv2.imshow('Hand Tracking', copy_image)
-
         
         if cv2.waitKey(5) & 0xFF == 27:
             break
-
 
 cap.release()
 cv2.destroyAllWindows()
