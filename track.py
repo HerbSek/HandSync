314cf55e248b3bc75c5390fc5c78667e7971ac4e
Herbert_dev <sekpeyherbert@gmail.com>
2024-07-03 00:22:02 +0000

    Making adjustments.

--- track.py
+++ track.py
@@ -1,44 +1,45 @@
 import cv2
-import numpy as np 
-# import pyautogui
-import time 
+# import numpy as np 
+# # import pyautogui
+# import time 
  
 
-time.sleep(5)
-cam = cv2.VideoCapture(0)
-width, height = pyautogui.size()
-cascade_face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+# time.sleep(5)
+# cam = cv2.VideoCapture(0)
+# width, height = pyautogui.size()
+# cascade_face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
 
-if not cam.isOpened():
-    raise IOError('Camera not detected')
+# if not cam.isOpened():
+#     raise IOError('Camera not detected')
  
-while True:
-    ret, frame = cam.read()
+# while True:
+#     ret, frame = cam.read()
 
-    if not ret:
-        break
+#     if not ret:
+#         break
    
-    frame = cv2.resize(frame, (int(width),int(height)) , interpolation= cv2.INTER_AREA)
-    frame = cv2.flip(frame,1)
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     frame = cv2.resize(frame, (int(width),int(height)) , interpolation= cv2.INTER_AREA)
+#     frame = cv2.flip(frame,1)
+#     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
 
-    load_cascade = cascade_face.detectMultiScale(grey, 1.2, 3) 
-    for (x,y,w,h) in load_cascade:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2) 
+#     load_cascade = cascade_face.detectMultiScale(grey, 1.2, 3) 
+#     for (x,y,w,h) in load_cascade:
+#         cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2) 
         
-        center_x = x + w // 2
-        center_y = y + h // 2
+#         center_x = x + w // 2
+#         center_y = y + h // 2
 
-        pyautogui.moveTo(center_x,center_y) 
+#         pyautogui.moveTo(center_x,center_y) 
         
-        print(f"Center coordinates: ({int(0.5*(w))}, {int(0.5*(h))})")
+#         print(f"Center coordinates: ({int(0.5*(w))}, {int(0.5*(h))})")
 
-    # cv2.imshow('frame', frame)
-    c = cv2.waitKey(100)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cam.release()
-cv2.destroyAllWindows()  
+#     # cv2.imshow('frame', frame)
+#     c = cv2.waitKey(100)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# cam.release()
+# cv2.destroyAllWindows()  
 
 
+print([x for x in dir(cv2) if x.startswith('COLOR_BGR2')])
--- vision_mouse.py
+++ vision_mouse.py
@@ -3,11 +3,10 @@ import mediapipe as mp
 import pyautogui
 import time
 
-def landmarks_close(lm1, lm2, threshold=0.05):
+def landmarks_close(lm1, lm2, threshold=0.10):
     return abs(lm1.x - lm2.x) < threshold and abs(lm1.y - lm2.y) < threshold and abs(lm1.z - lm2.z) < threshold
 
 
-# Initialize MediaPipe Hands solution
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
@@ -15,12 +14,11 @@ time.sleep(5)
 
 screen_width, screen_height = pyautogui.size()
 
-# Set up webcam input
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
         model_complexity=1,
-        min_detection_confidence=0.8,
-        min_tracking_confidence=0.8) as hands:
+        min_detection_confidence=0.6,
+        min_tracking_confidence=0.6) as hands:
     
     while cap.isOpened():
         success, image = cap.read()
@@ -28,26 +26,21 @@ with mp_hands.Hands(
             print("Ignoring empty camera frame.")
             continue
 
-        # Convert the BGR image to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Flip the image for a mirror effect
+
         image = cv2.flip(image, 1)
 
         copy_image = image.copy()
+        copy_image = cv2.cvtColor(copy_image, cv2.COLOR_RGB2BGR)
         copy_image = cv2.resize(copy_image, (screen_width//5, screen_height//5), interpolation = cv2.INTER_AREA)
 
-        image = cv2.resize(image, (int(screen_width * 1.2), int(screen_height * 1.2)), interpolation = cv2.INTER_AREA)
+        image = cv2.resize(image, (int(screen_width * 1.3), int(screen_height * 1.3)), interpolation = cv2.INTER_AREA)
 
-        
-
-        # Process the image and find hands
-        results = hands.process(image)
-
-        # Convert the image color back so it can be displayed
+    
+        results = hands.process(image)    
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Draw hand annotations on the image
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
@@ -66,29 +59,32 @@ with mp_hands.Hands(
 
                     # Optionally draw the coordinates on the image
                     # cv2.putText(image, f'{id}', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
-
-                    # Move the mouse to the landmark position
-                    if id == 8:  # Example: Move the mouse to the 9th landmark (index finger tip)
-                        cv2.circle(copy_image, (cx//5,cy//5), 1, (255, 0, 0), 3)
-                        pyautogui.moveTo(cx, cy)
+                   
+                   
+                    if not(landmarks_close(hand_landmarks.landmark[4], hand_landmarks.landmark[8])) :  # Example: Move the mouse to the 9th landmark (index finger tip)
+                        if id == 8:
+                            cv2.circle(copy_image, (cx//5,cy//5), 1, (255, 0, 0), 3)
+                            pyautogui.moveTo(cx, cy)
+                            print(f'Working !!! yay')
                             # time.sleep(0.1)  # Adding a small delay
-                        if landmarks_close(hand_landmarks.landmark[4], hand_landmarks.landmark[8]):
+                    elif landmarks_close(hand_landmarks.landmark[4], hand_landmarks.landmark[8]) and id == 8:
                         # if hand_landmarks.landmark[4] ==  hand_landmarks.landmark[8]:
-                            pyautogui.dragTo(cx, cy)
-                            print(f'User is dragging !!!')
+                        pyautogui.dragTo(cx, cy) 
+                        print(f'User is dragging !!!')
+                       
                     
                    
                     
 
 
-        # Display the annotated image
+        
         # cv2.imshow('Hand Tracking', image)
         cv2.imshow('Hand Tracking', copy_image)
 
-        # Break the loop when 'Esc' key is pressed
+        
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
-# Release the webcam and close the window
+
 cap.release()
 cv2.destroyAllWindows()
